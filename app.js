3a5c622e2e042d6e37617bcae99ee5bcce541f54
munyite001 <emunyite@gmail.com>
2022-11-08 08:53:56 +0300

    Fixed issues with container

--- app.js
+++ app.js
@@ -1,50 +1,46 @@
-const counter = document.querySelector('.count');
-const counterIcons = document.querySelectorAll('.counter-icon');
-const thumbnailImages = document.querySelectorAll('.thumbnail-img');
-const mainImage = document.querySelector('.main-image');
-const mainProductImage = document.querySelector('.main-product-image');
-const arrowIcons = document.querySelectorAll('.arrow-icon');
-const openMenu = document.querySelector('.open-btn');
-const closeMenu = document.querySelector('.close-menu');
-const overlay = document.querySelector('.overlay');
-const mobileMenu = document.querySelector('.mobile-menu');
-const cart = document.querySelector('.basket');
-const openCart = document.getElementById('cart-icon');
-const cartContent = document.querySelector('.cartContent');
-const cartQuantity = document.querySelector('.cart-items');
-const addToCart = document.querySelector('.add-to-cart');
+const counter = document.querySelector(".count");
+const counterIcons = document.querySelectorAll(".counter-icon");
+const thumbnailImages = document.querySelectorAll(".thumbnail-img");
+const mainImage = document.querySelector(".main-image");
+const mainProductImage = document.querySelector(".main-product-image");
+const arrowIcons = document.querySelectorAll(".arrow-icon");
+const openMenu = document.querySelector(".open-btn");
+const closeMenu = document.querySelector(".close-menu");
+const overlay = document.querySelector(".overlay");
+const mobileMenu = document.querySelector(".mobile-menu");
+const cart = document.querySelector(".basket");
+const openCart = document.getElementById("cart-icon");
+const cartContent = document.querySelector(".cartContent");
+const cartQuantity = document.querySelector(".cart-items");
+const addToCart = document.querySelector(".add-to-cart");
 
 const productImages = [
-`./images/image-product-1.jpg`,
-`./images/image-product-2.jpg`,
-`./images/image-product-3.jpg`,
-`./images/image-product-4.jpg`
+  `./images/image-product-1.jpg`,
+  `./images/image-product-2.jpg`,
+  `./images/image-product-3.jpg`,
+  `./images/image-product-4.jpg`,
 ];
 let totalPrice = 0;
 const cartArr = [];
 
 const cartItem = {
-  image: './images/image-product-1-thumbnail.jpg',
-  quantity: '',
-  title: 'Fall Limited Edition Sneakers',
+  image: "./images/image-product-1-thumbnail.jpg",
+  quantity: "",
+  title: "Fall Limited Edition Sneakers",
   price: 0,
-}
+};
 
 //  Counter
 
 let count = 0;
 
 counterIcons.forEach((icon) => {
-  icon.addEventListener('click', (e) => {
-    if (e.currentTarget.classList.contains('plus'))
-    {
+  icon.addEventListener("click", (e) => {
+    if (e.currentTarget.classList.contains("plus")) {
       count += 1;
       counter.innerHTML = count;
-    }
-    else
-    {
-      if(count > 0)
-      {
+    } else {
+      if (count > 0) {
         count -= 1;
         counter.innerHTML = count;
       }
@@ -54,46 +50,35 @@ counterIcons.forEach((icon) => {
 
 //  Thumbnail-Images
 thumbnailImages.forEach((image) => {
-  image.addEventListener('click', () => {
+  image.addEventListener("click", () => {
     thumbnailImages.forEach((img) => {
-      if (img.classList.contains('thumbnail-active'))
-      {
-        img.classList.remove('thumbnail-active');
+      if (img.classList.contains("thumbnail-active")) {
+        img.classList.remove("thumbnail-active");
       }
     });
-    image.classList.add('thumbnail-active');
+    image.classList.add("thumbnail-active");
     mainImage.innerHTML = `
     <img src="./images/image-product-${image.dataset.id}.jpg" alt="Product Image">
     `;
   });
 });
 
-
 //  Image Silder in Mobile
 let i = 0;
 const n = productImages.length;
 arrowIcons.forEach((icon) => {
-  icon.addEventListener('click', (e) => {
-    if (e.currentTarget.classList.contains('prev'))
-    {
-      if (i > 0)
-      {
+  icon.addEventListener("click", (e) => {
+    if (e.currentTarget.classList.contains("prev")) {
+      if (i > 0) {
         i -= 1;
-      }
-      else
-      {
-        i = n-1;
+      } else {
+        i = n - 1;
       }
       mainProductImage.src = `${productImages[i]}`;
-    }
-    else
-    {
-      if (i < n - 1)
-      {
+    } else {
+      if (i < n - 1) {
         i += 1;
-      }
-      else
-      {
+      } else {
         i = 0;
       }
       mainProductImage.src = `${productImages[i]}`;
@@ -101,34 +86,31 @@ arrowIcons.forEach((icon) => {
   });
 });
 
-
 //  Open and close Mobile Menu
-openMenu.addEventListener('click', () => {
-  overlay.classList.add('show-overlay');
-  mobileMenu.classList.add('show-mobile-menu');
+openMenu.addEventListener("click", () => {
+  overlay.classList.add("show-overlay");
+  mobileMenu.classList.add("show-mobile-menu");
 });
 
-closeMenu.addEventListener('click', () => {
-  overlay.classList.remove('show-overlay');
-  mobileMenu.classList.remove('show-mobile-menu');
+closeMenu.addEventListener("click", () => {
+  overlay.classList.remove("show-overlay");
+  mobileMenu.classList.remove("show-mobile-menu");
 });
 
 //  Basket
-openCart.addEventListener('click', () => {
-  cart.classList.toggle('show-basket');
+openCart.addEventListener("click", () => {
+  cart.classList.toggle("show-basket");
 });
 
-
 //  Cart Functionality
-addToCart.addEventListener('click', () => {
-  if (count > 0)
-  {
+addToCart.addEventListener("click", () => {
+  if (count > 0) {
     cartItem.quantity = count;
-    cartItem.price = 125.00;
+    cartItem.price = 125.0;
     totalPrice = (cartItem.price * count).toFixed(2);
     cartArr.push(cartItem);
     cartQuantity.textContent = count;
-    cartQuantity.style.display = 'block';
+    cartQuantity.style.display = "block";
     cartContent.innerHTML = `
     <div class="cartItem">
         <div class="cart-product-image">
@@ -136,27 +118,26 @@ addToCart.addEventListener('click', () => {
         </div>
         <div class="product-description">
           <h3>${cartItem.title}</h3>
-          <p>$${cartItem.price.toFixed(2)} x <span class="quantity">${cartItem.quantity}</span><span class="total-price">${totalPrice}</span></p>
+          <p>$${cartItem.price.toFixed(2)} x <span class="quantity">${
+      cartItem.quantity
+    }</span><span class="total-price">${totalPrice}</span></p>
         </div>
         <div class="delete">
           <svg width="14" height="16" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><defs><path d="M0 2.625V1.75C0 1.334.334 1 .75 1h3.5l.294-.584A.741.741 0 0 1 5.213 0h3.571a.75.75 0 0 1 .672.416L9.75 1h3.5c.416 0 .75.334.75.75v.875a.376.376 0 0 1-.375.375H.375A.376.376 0 0 1 0 2.625Zm13 1.75V14.5a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 1 14.5V4.375C1 4.169 1.169 4 1.375 4h11.25c.206 0 .375.169.375.375ZM4.5 6.5c0-.275-.225-.5-.5-.5s-.5.225-.5.5v7c0 .275.225.5.5.5s.5-.225.5-.5v-7Zm3 0c0-.275-.225-.5-.5-.5s-.5.225-.5.5v7c0 .275.225.5.5.5s.5-.225.5-.5v-7Zm3 0c0-.275-.225-.5-.5-.5s-.5.225-.5.5v7c0 .275.225.5.5.5s.5-.225.5-.5v-7Z" id="a"/></defs><use fill="#C3CAD9" fill-rule="nonzero" xlink:href="#a"/></svg>
         </div>
       </div>
       <button type="button" class="btn">Checkout</button>`;
-      const deleteCart = document.querySelector('.delete');
-      //  Delete from Cart
-      deleteCart.addEventListener('click', ()=>{
-        cartArr.pop();
-        cartContent.innerHTML = `<p>Your Cart is empty.</p>`;
-        cartQuantity.style.display = 'none';
-
-      });
+    const deleteCart = document.querySelector(".delete");
+    //  Delete from Cart
+    deleteCart.addEventListener("click", () => {
+      cartArr.pop();
+      cartContent.innerHTML = `<p>Your Cart is empty.</p>`;
+      cartQuantity.style.display = "none";
+    });
   }
 });
 
-if (cartArr.length == 0)
-{
+if (cartArr.length == 0) {
   cartContent.innerHTML = `<p>Your Cart is empty.</p>`;
-  cartQuantity.style.display = 'none';
+  cartQuantity.style.display = "none";
 }
-
